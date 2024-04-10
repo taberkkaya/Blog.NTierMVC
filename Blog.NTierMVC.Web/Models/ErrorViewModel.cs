b1392daf8d181a801034708d88738d02a5e3f19e
taberkkaya <taberkkaya@gmail.com>
2024-04-10 03:32:44 +0300

    Add project files.

---
+++ Blog.NTierMVC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blog.NTierMVC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
