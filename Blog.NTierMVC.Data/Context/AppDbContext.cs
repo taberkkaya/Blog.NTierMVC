6a24bb6e96637d205ecc6e76a4aac13d83f9014d
taberkkaya <taberkkaya@gmail.com>
2024-04-11 03:12:28 +0300

    Entities created and add first mig

---
+++ Blog.NTierMVC.Data/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Blog.NTierMVC.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.NTierMVC.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+            
+        }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}
--- Blog.NTierMVC.Web/Program.cs
+++ Blog.NTierMVC.Web/Program.cs
@@ -1,7 +1,12 @@
+using Blog.NTierMVC.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
