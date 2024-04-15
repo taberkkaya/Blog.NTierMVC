3ea706a5fdcd461bda6a62ff222fe6574c45ae5d
atkaya <atkaya>
2024-04-15 10:00:01 +0300

    UnitOfWork added

--- Blog.NTierMVC.Data/Extensions/DataLayerExtensions.cs
+++ Blog.NTierMVC.Data/Extensions/DataLayerExtensions.cs
@@ -1,14 +1,10 @@
 ﻿using Blog.NTierMVC.Data.Context;
 using Blog.NTierMVC.Data.Repositories.Abstractions;
 using Blog.NTierMVC.Data.Repositories.Conretes;
+using Blog.NTierMVC.Data.UnitOfWorks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blog.NTierMVC.Data.Extensions
 {
@@ -17,7 +13,11 @@ namespace Blog.NTierMVC.Data.Extensions
         public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             return services;
         }
     }
