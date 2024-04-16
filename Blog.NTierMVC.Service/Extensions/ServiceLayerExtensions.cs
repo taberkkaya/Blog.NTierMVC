d484bab3b9fed6edf6a343a861a2fc38bd67ca7f
taberkkaya <taberkkaya@gmail.com>
2024-04-16 22:45:52 +0300

    ArticleService created and tested

---
+++ Blog.NTierMVC.Service/Extensions/ServiceLayerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Blog.NTierMVC.Service.Service.Abstractions;
+using Blog.NTierMVC.Service.Service.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog.NTierMVC.Service.Extensions
+{
+    public static class ServiceLayerExtensions
+    {
+        public static IServiceCollection LoadServiceLayerExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IArticleService,ArticleService>();
+
+            return services;
+        }
+    }
+}
--- Blog.NTierMVC.Web/Controllers/HomeController.cs
+++ Blog.NTierMVC.Web/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Blog.NTierMVC.Service.Service.Abstractions;
 using Blog.NTierMVC.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,15 +8,18 @@ namespace Blog.NTierMVC.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IArticleService articleService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IArticleService articleService)
         {
             _logger = logger;
+            this.articleService = articleService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var articles = await articleService.GetAllArticlesAsync();
+            return View(articles);
         }
 
         public IActionResult Privacy()
