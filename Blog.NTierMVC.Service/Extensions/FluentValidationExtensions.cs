b9864f07b4f9b98fc7e17a770810fd443c887e7f
atkaya <atkaya>
2024-04-30 15:00:27 +0300

    Fluent Validation added to add article and update article ops

---
+++ Blog.NTierMVC.Service/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.NTierMVC.Service.Extensions
+{
+    public static class FluentValidationExtensions
+    {
+        public static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState)
+        {
+            foreach(var error in result.Errors)
+            {
+                modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+        }
+    }
+}
