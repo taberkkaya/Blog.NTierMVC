31ac2c80f73e9cf6cb331c7e0bd6824f3f0e9cb5
atkaya <atkaya>
2024-05-02 14:17:31 +0300

    Find logged in user

---
+++ Blog.NTierMVC.Service/Extensions/LoggedInUserExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.NTierMVC.Service.Extensions
+{
+    public static class LoggedInUserExtensions
+    {
+        public static Guid GetLoggedInUserId(this ClaimsPrincipal principal)
+        {
+            return Guid.Parse(principal.FindFirstValue(ClaimTypes.NameIdentifier));
+        }
+
+        public static string GetLoggedInUserEmail(this ClaimsPrincipal principal) {
+            return principal.FindFirstValue(ClaimTypes.Email);
+        }
+    }
+}
