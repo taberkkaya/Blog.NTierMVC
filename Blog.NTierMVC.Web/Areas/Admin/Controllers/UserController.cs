1209828f6e934e2042fea3bbfe88814b5d296cb2
taberkkaya <taberkkaya@gmail.com>
2024-05-04 21:30:10 +0300

    All user listed

---
+++ Blog.NTierMVC.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Blog.NTierMVC.Entity.DTOs.Users;
+using Blog.NTierMVC.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.NTierMVC.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly IMapper mapper;
+
+        public UserController(UserManager<AppUser> userManager,IMapper mapper)
+        {
+            this.userManager = userManager;
+            this.mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.ToListAsync();
+            var map = mapper.Map<List<UserDto>>(users);
+
+            foreach (var user in map)
+            {
+                var findUser = await userManager.FindByIdAsync(user.Id.ToString());
+                var role = string.Join("", await userManager.GetRolesAsync(findUser));
+
+                user.Role = role;
+            }
+            
+            return View(map);
+        }
+    }
+}
