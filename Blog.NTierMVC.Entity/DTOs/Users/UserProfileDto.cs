9bc6ff0eef779aaad5cabfcdb6f0dd4ab51c845b
atkaya <atkaya>
2024-05-06 15:58:22 +0300

    #34 38:52 User Profile done

---
+++ Blog.NTierMVC.Entity/DTOs/Users/UserProfileDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.NTierMVC.Entity.DTOs.Users
+{
+    public class UserProfileDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CurrentPassword { get; set; }
+        public string? NewPassword { get; set; }
+
+    }
+}
