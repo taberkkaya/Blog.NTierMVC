258485bca84e6d9de45d6f888bf9d5f37e1803a0
atkaya <atkaya>
2024-08-02 17:22:30 +0300

    some changes

---
+++ Blog.NTierMVC.Web/ViewComponents/HomeRecentPostViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Blog.NTierMVC.Service.Service.Abstractions;
+using Blog.NTierMVC.Service.Service.Concretes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.NTierMVC.Web.ViewComponents
+{
+    public class HomeRecentPostViewComponent : ViewComponent
+    {
+        private readonly IArticleService articleService;
+
+        public HomeRecentPostViewComponent(IArticleService articleService)
+        {
+            this.articleService = articleService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+         
+            var articles = await articleService.GetAllArticlesWithCategoryNonDeletedAsync();
+
+            var sortedLast3Article = articles.OrderBy(x => x.CreatedDate).Take(3).ToList();
+
+            return View(sortedLast3Article);
+        }
+    }
+}
