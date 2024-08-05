c8c19b52c619beac067dc6870d0ec739208b2f8a
taberkkaya <taberkkaya@taberkkaya.com>
2024-08-05 19:56:12 +0300

    Finished

---
+++ Blog.NTierMVC.Web/ViewComponents/LayoutFooterSocialMediaViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Blog.NTierMVC.Service.Service.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.NTierMVC.Web.ViewComponents
+{
+    public class LayoutFooterSocialMediaViewComponent : ViewComponent
+    {
+        private readonly ISocialMediaService mediaService;
+
+        public LayoutFooterSocialMediaViewComponent(ISocialMediaService mediaService)
+        {
+            this.mediaService = mediaService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var socialMedias = await mediaService.GetAllSocialMediasNonDeleted();
+            return View(socialMedias);
+        }
+    }
+}
